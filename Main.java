3e98e141e5da94787c2d3219282b30e79f30b8f6
Shintya123 <shintyatogatorop9@gmail.com>
2026-04-10 22:21:44 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Goblin goblin = new Goblin("Goblin Kecil", 50);
-        Dragon dragon = new Dragon("Naga Api", 200, 75);
-        goblin.monsterSound();
-        dragon.monsterSound();
-    }
-}
