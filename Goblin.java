41c087a5acfb172a2cf79b7fbdf8c042443b4552
Shintya123 <shintyatogatorop9@gmail.com>
2026-04-10 22:21:35 +0700

    Delete Goblin.java

--- Goblin.java
+++
@@ -1,9 +0,0 @@
-public class Goblin extends Monster {
-    public Goblin(String name, int baseHp) {
-        super(name, baseHp);
-    }
-    @Override
-    public void monsterSound() {
-        System.out.println("Kikikik! Goblin mengendap-endap!");
-    }
-}
