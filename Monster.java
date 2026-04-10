8fea40ef13d024067e22c8cbef0ee50032eb1ae7
Shintya123 <shintyatogatorop9@gmail.com>
2026-04-10 22:22:11 +0700

    Create Monster.java

---
+++ Monster.java
@@ -0,0 +1,13 @@
+public class Monster {
+    protected String name;
+    protected int baseHp;
+
+    public Monster(String name, int baseHp) {
+        this.name = name;
+        this.baseHp = baseHp;
+    }
+
+    public void monsterSound() {
+        System.out.println("Grooaarrr! Monster mendekat!");
+    }
+}
